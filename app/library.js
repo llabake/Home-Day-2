81245c0dba243406ccfcb9a26d824eff64e0acb7
llabake <lemboyelabake@gmail.com>
2017-06-07 16:53:47 +0100

    Implementation of car class in oop

--- app/library.js
+++ app/library.js
@@ -1,24 +1,43 @@
 
-
 module.exports = {
-
-    reverseString: function (str) {
-        new_str = [];
-
-        if (str.length === 0) {
-            return null;
-        } else if (typeof (str) != 'string') {
-            return null;
+    Car:  function(name, model, type)  {
+        if (!name) {
+            this.name = 'General'
         } else {
-            for (var i = 0; i < str.length; i++) {
-                new_str.unshift(str[i]);
-            }
-            new_str = new_str.join('');
-            if (new_str === str) {
-                return true;
+            this.name = name
+        }
+        
+        if (!model) {
+            this.model = 'GM'
+        } else {
+            this.model = model
+        }
+                
+        if ((this.name ==='Porshe') || (this.name === 'Koenigsegg')) {
+            this.numOfDoors = 2
+        } else {
+            this.numOfDoors = 4
+        }
+        
+        this.type = type;
+        
+        if(this.type != 'trailer'){
+            this.numOfWheels = 4;
+            this.isSaloon = true;
+        } else{
+            this.numOfWheels = 8;
+            this.isSaloon = false;
+        }
+        
+        
+        this.speed = '0 km/h'
+        
+        this.drive = function (speed) {
+            if(this.type === 'trailer'){    
+                this.speed = '77 km/h';
             } else {
-                return new_str;
+                this.speed = '250 km/h'
             }
+            return this;
         }
     }
-}
